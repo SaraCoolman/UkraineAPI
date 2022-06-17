72596aaa765392cc72d86d929d0c5585168aa1f6
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-17 12:31:48 +0200

    Update db.config.js

--- api/app/config/db.config.js
+++ api/app/config/db.config.js
@@ -8,7 +8,7 @@ module.exports = {
     database: 'ukrainelodgeservice',
     port: 3306,
     dialect: 'mysql',
-    ssl: { ca: fs.readFileSync("C:/Users/Sara/Downloads/DigiCertGlobalRootCA.crt.pem") },
+    ssl: { ca: fs.readFileSync("https://github.com/SaraCoolman/UkraineAPI/blob/main/DigiCertGlobalRootCA.crt.pem") },
     pool: {
         max: 5,
         min: 0,
