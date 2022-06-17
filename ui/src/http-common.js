41a96b6a50617b3f2c2cc1c0658bcb7591724122
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-17 19:57:25 +0200

    deleted slash

--- ui/src/http-common.js
+++ ui/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.get('http://localhost:8081')
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://ukraine-api.herokuapp.com/',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://ukraine-api.herokuapp.com',
   headers: {
     "Content-type": "application/json"
   }
