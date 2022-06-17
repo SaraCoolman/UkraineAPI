6fe01bb4b13e7810eb15db7453cce5c368a5df11
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-17 17:39:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const db = require("./app/models");
+const db = require("./api/app/models");
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
