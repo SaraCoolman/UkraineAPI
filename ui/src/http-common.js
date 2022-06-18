b3391d7fb8c88745faf6b7c191554b15894f3b04
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-18 11:09:55 +0200

    common js

--- ui/src/http-common.js
+++ ui/src/http-common.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.get('https://62ad8ed96d0676527f26bef3--whimsical-brioche-26147f.netlify.app')
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://ukraine-api.herokuapp.com',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://ukraine-api.herokuapp.com/api',
   headers: {
     "Content-type": "application/json"
   }
