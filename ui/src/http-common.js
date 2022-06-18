0a9ce67f78b947d9ffeef9b2a278064a6a113da4
SaraCoolman <56956632+SaraCoolman@users.noreply.github.com>
2022-06-18 10:56:56 +0200

    Update http-common.js

--- ui/src/http-common.js
+++ ui/src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.get('http://localhost:8081')
+axios.get('https://62ad8ed96d0676527f26bef3--whimsical-brioche-26147f.netlify.app')
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'https://ukraine-api.herokuapp.com',
